ba2f8c6e9595af2fc96eb69d20af28eaa88efc19
Jesús <csi22-jalvugo@colegioaltair.net>
2024-01-15 13:37:01 +0100

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,28 @@
+﻿using Ejercicio1String.Servicios;
+
+namespace Ejercicio1String.Controladores
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string nombre;
+            string apellido1;
+            string apellido2;
+            OperativaInterfaz oi = new OperativaImplementacion();
+
+            nombre = oi.pedirNombreOApe(0);
+            apellido1 = oi.pedirNombreOApe(1);
+            apellido2 = oi.pedirNombreOApe(2);
+
+            while (nombre == apellido1 || nombre == apellido2)
+            {
+                Console.WriteLine("ERROR! su nombre coincide con alguno de sus apellidos.");
+
+                nombre = oi.pedirNombreOApe(0);
+                apellido1 = oi.pedirNombreOApe(1);
+                apellido2 = oi.pedirNombreOApe(2);
+            }
+        }
+    }
+}
---
+++ Servicios/OperativaImplementacion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1String.Servicios
+{
+    internal class OperativaImplementacion : OperativaInterfaz
+    {
+        public string pedirNombreOApe(int controladorMens)
+        {
+            string nombreVal=" ";
+            
+            if(controladorMens == 0)
+            {
+                Console.WriteLine("Introduzca su nombre: ");
+                nombreVal = Console.ReadLine();
+            }else if(controladorMens == 1)
+            {
+                Console.WriteLine("Introduzca su primer apellido");
+                nombreVal = Console.ReadLine();
+            }else if (controladorMens == 2)
+            {
+                Console.WriteLine("Introduzca su segundo apellido");
+                nombreVal = Console.ReadLine();
+            }
+
+            return nombreVal;
+
+
+        }
+    }
+}
